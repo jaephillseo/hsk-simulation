83890e8322fe56f572fb57ebdb8156e485b82203
JP <jaephillseo@gmail.com>
2025-03-28 10:27:06 +0700

    fix: bind Flask to Render's PORT

--- app.py
+++ app.py
@@ -203,5 +203,9 @@ app.jinja_env.globals.update(str=str)
 def download_schedule():
     return send_file(GENERATED_FILE_PATH, as_attachment=True, download_name="daily_schedule.xlsx")
 
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
